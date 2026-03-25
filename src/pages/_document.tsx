3d1a05e57a03b70a70970033ee56951b37259a9f
evilbeliver <82883551+evilbeliver@users.noreply.github.com>
2026-03-25 15:19:38 -0400

    updated documents

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,12 @@ export default function Document() {
         <link rel="icon" href="/images/weim-favicon.png" />
         <link rel="apple-touch-icon" href="/images/weim-favicon.png" />
         <link rel="shortcut icon" href="/images/weim-favicon.png" />
+        {/* Preconnect to external resources for Petfinder widget */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link rel="preconnect" href="https://www.petfinder.com" />
+        <link rel="preconnect" href="https://psl.petfinder.com" />
+        <link rel="preconnect" href="https://dbw3zep4prcju.cloudfront.net" />
       </Head>
       <body>
         <Main />
